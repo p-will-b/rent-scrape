4467d44ad93300c16f7f878ec11758288e98ced8
p-will-b <peter.w.beer@gmail.com>
2022-06-29 20:02:42 -0700

    reset json search bc it failed w rds

--- R/avalon-json-search.R
+++ R/avalon-json-search.R
@@ -1,7 +1,6 @@
 library(jsonlite)
 library(httr)
 library(xml2)
-library(rvest)
 library(tidyverse)
 
 # create randomized time
@@ -95,89 +94,13 @@ get_avalon_website <- possibly(
   otherwise = xml_new_document()
 )
 
-
 write_prop_xml <- function(xml_doc, property_name) {
   write_xml(xml_doc, sprintf("./data/%s_%s-prop-data.xml", Sys.Date(), property_name))
 }
 
-save_prop_rds <- function(df, property_name) {
-  saveRDS(df, sprintf("./data/%s_%s-prop-data.rds", Sys.Date(), property_name))
-}
-
-# parsing function
-
-html_json <- function(website_xml) {
-  website_xml %>%
-    html_nodes(xpath = "//script[@id='fusion-metadata']") %>%
-    html_text()
-}
-
-parse_html_json <- function(json_text) {
-  str_extract(json_text, "(?<=Fusion\\.globalContent\\=).+?(?=;Fusion\\.globalContentConfig)") %>%
-    fromJSON()
-}
-
-get_prop_unit_info <- function(json_list, element_name, col_names_general, cols_to_drop_unit) {
-
-  general_info <- bind_rows(unlist(json_list[names(json_list) %in% col_names_general])) %>%
-    rename(propertyName = name, propertyArea = area)
-
-  df_start <- json_list[[element_name]] %>%
-    mutate(
-      floorPlanId = floorPlan$name,
-      propertyAddress = address$address
-    )
-
-  df_units <- df_start[, !colnames(df_start) %in% cols_to_drop_unit] %>%
-    unnest(cols = lowestPricePerMoveInDate, names_sep = "_") %>%
-    unnest(cols = unitRentPrice) %>%
-    unnest(cols = pricesPerMoveinDate) %>%
-    unnest(pricesPerTerms, names_sep = "_") %>%
-    pivot_longer(matches("pricesPerTerms_\\d+"), names_to = "leaseTerm", values_to = "data") %>%
-    unnest(data) %>%
-    mutate(
-      leaseTerm = as.numeric(gsub("pricesPerTerms_", "", leaseTerm, fixed = T)),
-      across(c(matches("Date(_date)?$")), ~ lubridate::mdy(gsub("(\\d+\\/\\d+/\\d+)(.*)", "\\1", .x)))
-    )
-
-  prop_final <- bind_cols(general_info, df_units)
-  return(prop_final)
-
-}
-
-# safe version
-
-parse_prop_xml <- function(website_xml_file, element_name, col_names_general, cols_to_drop_unit) {
-
-  parsed_json <- html_json(website_xml = website_xml_file) %>%
-    parse_html_json()
-
-  get_prop_unit_info(
-    parsed_json,
-    element_name,
-    col_names_general,
-    cols_to_drop_unit
-  ) %>%
-    mutate(as_of_date = Sys.Date())
-
-}
-
-possibly_parse_prop_xml <- possibly(parse_prop_xml, otherwise = tibble())
-
-# loop through, write xml, save rds
-
 walk(
   1:nrow(prop_links), ~ {
-    avw <- get_avalon_website(target_link = prop_links$links[.x])
-    write_prop_xml(xml_doc = avw, property_name = prop_links$property[.x])
-    parse_prop_xml(
-      website_xml_file = avw,
-      element_name = "units",
-      col_names_general = c("communityId", "name", "area", "geoLocation", "phone", "email"),
-      cols_to_drop_unit = c("floorPlan", "address", "virtualTour", "characteristics", "promotions")
-    ) %>%
-      save_prop_rds(property_name = prop_links$property[.x])
-    avw <- NULL
-
+    get_avalon_website(target_link = prop_links$links[.x]) %>%
+      write_prop_xml(property_name = prop_links$property[.x])
   }
 )
